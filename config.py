f115a26e57255f66f553fa955d11d1b89e5f1180
guangyuzhang <13621160019@163.com>
2020-09-28 12:00:53 +0800

    updated topic and payload

--- config.py
+++ config.py
@@ -1,22 +1,29 @@
 myems_system_db = {
     'user': 'root',
-    'password': 'MyPassword',
-    'host': '192.168.0.1',
+    'password': '!MyEMS1',
+    'host': '127.0.0.1',
     'database': 'myems_system_db',
 }
 
 myems_historical_db = {
     'user': 'root',
-    'password': 'MyPassword',
-    'host': '192.168.0.1',
+    'password': '!MyEMS1',
+    'host': '127.0.0.1',
     'database': 'myems_historical_db',
 }
 
 myems_mqtt_broker = {
-    'host': '192.168.0.1',
+    'host': '127.0.0.1',
     'port': 1883,
     'username': 'admin',
     'password': 'Password1',
 }
+
+# The quality of service level to use.
+# The value is one of 0, 1 or 2,
+qos = 0
+
+# The topic prefix that the message should be published on.
+topic_prefix = 'myems/point/'
 
 period_in_seconds = 60
